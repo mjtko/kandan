73f90945fb10b2b3dd4f27272ebecbed1dab8ec1
Mark J. Titorenko <mark.titorenko@alces-software.com>
2013-02-25 19:34:38 +0000

    include key length for mysql compat

--- db/schema.rb
+++ db/schema.rb
@@ -83,6 +83,6 @@ ActiveRecord::Schema.define(:version => 20130221173650) do
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["ido_id"], :name => "index_users_on_ido_id", :unique => true
+  add_index "users", ["ido_id"], :name => "index_users_on_ido_id", :unique => true, :length => {"ido_id"=>255}
 
 end
